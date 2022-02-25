3beabeb5fcf87ebd08c16e00c1a13d4d67ea28ad
easy-quest <esim.i2p@gmail.com>
2022-02-25 13:48:21 +0000

    commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+
+if test ! (command -v yarn)
+    yarn install
+end
+
+set patth (command -v tor)
+sudo cp $patth .
+
+set patth (command -v node)
+sudo cp $patth .
+
+./run.sh
